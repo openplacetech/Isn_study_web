4ef3353f6f2333bf55bac3985efc4991192474f7
pramish karkee <pramishkarkee@pramishs-Air.mshome.net>
2024-07-17 11:25:30 +0545

    push

--- ISNstudy_web/settings.py
+++ ISNstudy_web/settings.py
@@ -123,6 +123,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 MEDIA_URL = '/media/'
+STATIC_ROOT = "/var/www/"
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
