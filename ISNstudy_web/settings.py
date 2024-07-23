c55b61b60cfe8d70e54994083f002ba6233068d0
krishnakarki <krishnakarki.mail@gmail.com>
2024-07-23 15:24:26 -0500

    update for static dir

--- ISNstudy_web/settings.py
+++ ISNstudy_web/settings.py
@@ -28,7 +28,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv('SECRET_KEY',"psdfbsdkjfhsdbfsdvfnbdsvfbnsdvf")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.getenv('DEBUG',False)
+DEBUG = os.getenv('DEBUG',True)
 
 ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', "*")
 ALLOWED_HOSTS = [host.strip() for host in ALLOWED_HOSTS.split(',') if host.strip()]
@@ -126,8 +126,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = '/static/'
-#STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+if DEBUG:
+    STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+else:
+    STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
 #
 MEDIA_URL = '/media/'
 
