0849d7fc468d4259dfc1d6c61dfaea712449fb86
krishnakarki <krishnakarki.mail@gmail.com>
2024-07-23 13:57:46 -0500

    update for static dir

--- ISNstudy_web/settings.py
+++ ISNstudy_web/settings.py
@@ -129,7 +129,7 @@ STATIC_URL = '/static/'
 if DEBUG:
     STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 else:
-    STATIC_ROOT = os.path.join(BASE_DIR,'static')
+    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 #
 MEDIA_URL = '/media/'
 
