26b5fc3d4cb956a6a0e9b63ec2cc14d6424e109d
pramish karkee <pramishkarkee@pramishs-Air.mshome.net>
2024-07-24 23:04:14 +0545

    push

--- ISNstudy_web/settings.py
+++ ISNstudy_web/settings.py
@@ -28,7 +28,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv('SECRET_KEY',"psdfbsdkjfhsdbfsdvfnbdsvfbnsdvf")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.getenv('DEBUG',True)
+# DEBUG = os.getenv('DEBUG',False)
+DEBUG = True
 
 ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', "*")
 ALLOWED_HOSTS = [host.strip() for host in ALLOWED_HOSTS.split(',') if host.strip()]
