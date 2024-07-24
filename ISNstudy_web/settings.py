20004b4eeb8c8c62ae4bab49bca7d19b288f949a
pramish karkee <pramishkarkee@pramishs-Air.mshome.net>
2024-07-24 23:10:17 +0545

    push

--- ISNstudy_web/settings.py
+++ ISNstudy_web/settings.py
@@ -190,7 +190,7 @@ TINYMCE_DEFAULT_CONFIG = {
 
 #email setup
 
-EMAIL_BACKEND = os.getenv('EMAIL_BACKEND')
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 EMAIL_HOST = os.getenv('EMAIL_HOST')
 EMAIL_PORT = os.getenv('EMAIL_USE_TLS')
 EMAIL_USE_TLS = os.getenv('EMAIL_USE_TLS')
